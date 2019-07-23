d1bfbbed36a036b68ca2d3ed675ad6f197f75492
Tobias <33788883+tobyornottobe@users.noreply.github.com>
2019-07-23 23:22:30 +0200

    updated contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 import 'openzeppelin-solidity/contracts/lifecycle/Pausable.sol';
 
-
+/*
 contract SimpleStorage {
   uint storedData;
 
@@ -14,4 +14,54 @@ contract SimpleStorage {
   function get() public view returns (uint) {
     return storedData;
   }
+}
+*/
+
+
+contract UBI {
+
+    address agent;
+
+    mapping(address => uint256) public deposits;
+
+    uint Now;
+    uint payTime;
+
+    modifier onlyAgent() {
+        require(msg.sender == agent);
+        _;
+    }
+
+    modifier onlyOnPayDay() {
+        require (block.timestamp > payTime);
+        _;
+    }
+
+    constructor() public {
+        agent = msg.sender;
+    }
+
+    function claimTime() public {
+         Now = now;
+         payTime = Now + 10 seconds;
+    }
+
+    function deposit(address beneficiary) public onlyAgent payable {
+        uint256 amount = msg.value;
+        deposits[beneficiary] = deposits[beneficiary] + amount;
+    }
+
+    function withdraw(address payable beneficiary) public onlyOnPayDay onlyAgent
+
+    {
+        uint256 payment = deposits[beneficiary];
+        deposits[beneficiary] = 0;
+        claimTime();
+        beneficiary.transfer(payment);
+
+    }
+
+
+
+
 }
